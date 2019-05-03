ee24ad67f51726878cf6a81257efe6db11f4dcf1
Alumno <Alumno@PC-INFOR00>
2019-05-03 11:05:56 +0200

    Reparacion .properties de la base de datos implementado en la bcapa web

--- src/com/eddie/ecommerce/dao/Utils/ConnectionManager.java
+++ src/com/eddie/ecommerce/dao/Utils/ConnectionManager.java
@@ -2,6 +2,7 @@ package com.eddie.ecommerce.dao.Utils;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,15 +13,17 @@ public class ConnectionManager	 {
 	
 	private static Logger logger = LogManager.getLogger(ConnectionManager.class.getName());
 	
+	private static ResourceBundle dbConfiguration = ResourceBundle.getBundle("DBConfiguration");
 	//IP clase:10.53.124.212:3306
-	private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-	private static final String DB_URL = "jdbc:mysql://localhost/powergaming?"
-			+ "useUnicode=true&useJDBCCompliantTimezoneShift=true"
-			+ "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static final String DRIVER_CLASS_NAME_PARAMETER = "jdbc.driver.classname";
+	private static final String URL_PARAMETER = "jdbc.url";
+	private static final String USER_PARAMETER = "jdbc.user";
+	private static final String PASSWORD_PARAMETER = "jdbc.password";
 
 	//  Database credentials
-	private static final String USER = "root";
-	private static final String PASS = "root";
+	private static String url;
+	private static String user;
+	private static String password;
 	private static final Integer TAMANHOMAX= 50;
 	private static final Integer TAMANHOMIN=1;
 	
@@ -31,12 +34,16 @@ public class ConnectionManager	 {
 		try {
 			// Carga el driver directamente, sin pool 
 			//Class.forName(JDBC_DRIVER);
+			String driverClassName = dbConfiguration.getString(DRIVER_CLASS_NAME_PARAMETER);
+			url = dbConfiguration.getString(URL_PARAMETER);
+			user = dbConfiguration.getString(USER_PARAMETER);
+			password = dbConfiguration.getString(PASSWORD_PARAMETER);
 			
 			 poolConexiones = new ComboPooledDataSource();
-			 poolConexiones.setDriverClass(JDBC_DRIVER);
-			 poolConexiones.setJdbcUrl(DB_URL);
-			 poolConexiones.setUser(USER);
-			 poolConexiones.setPassword(PASS);
+			 poolConexiones.setDriverClass(driverClassName);
+			 poolConexiones.setJdbcUrl(url);
+			 poolConexiones.setUser(user);
+			 poolConexiones.setPassword(password);
 			 poolConexiones.setMinPoolSize(TAMANHOMIN);
 			 poolConexiones.setMaxPoolSize(TAMANHOMAX);
 			 
